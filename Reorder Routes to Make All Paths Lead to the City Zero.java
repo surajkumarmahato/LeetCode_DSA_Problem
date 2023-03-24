020f47608081d90d653b4c46d1af946a4c7df7c0
SURAJ KUMAR MAHATO <suraj.mahato.4166@gmail.com>
2023-03-24 19:52:03 +0530

    Reorder Routes to Make All Paths Lead to the City Zero Added

---
+++ Reorder Routes to Make All Paths Lead to the City Zero.java
@@ -0,0 +1,35 @@
+/*
+Reorder Routes to Make All Paths Lead to the City Zero
+
+There are n cities numbered from 0 to n - 1 and n - 1 roads such that there is only one way to travel between two different cities (this network form a tree). 
+Last year, The ministry of transport decided to orient the roads in one direction because they are too narrow.
+Roads are represented by connections where connections[i] = [ai, bi] represents a road from city ai to city bi.
+This year, there will be a big event in the capital (city 0), and many people want to travel to this city.
+Your task consists of reorienting some roads such that each city can visit the city 0. Return the minimum number of edges changed.
+It's guaranteed that each city can reach city 0 after reorder.
+*/
+
+class Solution 
+{
+   int dfs(List<List<Integer>> al, boolean[] visited, int from) 
+   {
+        int change = 0;
+        visited[from] = true;
+        for (var to : al.get(from))
+            if (!visited[Math.abs(to)])
+                change += dfs(al, visited, Math.abs(to)) + (to > 0 ? 1 : 0);
+        return change;   
+    }
+    public int minReorder(int n, int[][] connections) 
+    {
+        List<List<Integer>> al = new ArrayList<>();
+        for(int i = 0; i < n; ++i) 
+            al.add(new ArrayList<>());
+        for (var c : connections) 
+        {
+            al.get(c[0]).add(c[1]);
+            al.get(c[1]).add(-c[0]);
+        }
+        return dfs(al, new boolean[n], 0);
+    }
+}
